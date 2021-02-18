76db10fdb371a602e572cf3b23c05f023a4c00d1
Void <a>
2021-02-18 21:02:05 +0000

    Created testing controls

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float lateralSpeed = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("PlayerMovement.cs started");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+          rb.AddForce(lateralSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+          rb.AddForce(-lateralSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
